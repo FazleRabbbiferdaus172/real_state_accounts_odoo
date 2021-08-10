b2389f2716c1a68c1b0fe87d44d2f2a38bfb01db
ferdaus-bs23 <Rabbi.Ferdaus@brainstation-23.com>
2021-08-10 17:45:30 +0600

    can create invoices invoices

--- models/estate_property.py
+++ models/estate_property.py
@@ -6,6 +6,19 @@ class Property(models.Model):
 
     def action_sold(self):
         print("i work :)")
-        journal = self.env['account.invoice'].create({'partner_id': self.buyer_id.id, 'move_type':'out_invoice', 'journal_id': 0})
-        # journal = self.env['account.move'].with_context(default_move_type='out_invoice', partner_id=self.buyer_id.id)._get_default_journal()
-        
+        journal = self.env['account.invoice'].create({'partner_id': self.buyer_id.id, 'move_type':'out_invoice', 'journal_id':1, 'account_id': 1,
+        "invoice_line_ids": [
+                (
+                    0,
+                    0,
+                    {
+                        "name": self.name,
+                        "quantity": 1,
+                        "price_unit": self.selling_price,
+                        'account_id': 1,
+                        'tax_ids': 0,
+                    },
+                )
+            ],})
+        # journal = self.env['account.move'].with_context(default_move_type='out_invoice')._get_default_journal()
+        return super().action_sold()
